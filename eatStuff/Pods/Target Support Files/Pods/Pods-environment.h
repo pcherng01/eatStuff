e9c7f4133a4a3a5a595c1c3408819f66743bc29e
pcherng01 <LeafCherngchaosil@Pongsakorns-MacBook-Pro.local>
2015-03-01 08:44:38 -0800

    working code after Pod installing

---
+++ eatStuff/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,92 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
+// AFNetworking/NSURLConnection
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 1
+
+// AFNetworking/NSURLSession
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 1
+
+// AFNetworking/Reachability
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 1
+
+// AFNetworking/Security
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 1
+
+// AFNetworking/Serialization
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 1
+
+// AFNetworking/UIKit
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 5
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
+
+// Braintree
+#define COCOAPODS_POD_AVAILABLE_Braintree
+#define COCOAPODS_VERSION_MAJOR_Braintree 3
+#define COCOAPODS_VERSION_MINOR_Braintree 6
+#define COCOAPODS_VERSION_PATCH_Braintree 1
+
+// Braintree/API
+#define COCOAPODS_POD_AVAILABLE_Braintree_API
+#define COCOAPODS_VERSION_MAJOR_Braintree_API 3
+#define COCOAPODS_VERSION_MINOR_Braintree_API 6
+#define COCOAPODS_VERSION_PATCH_Braintree_API 1
+
+// Braintree/Drop-In
+#define COCOAPODS_POD_AVAILABLE_Braintree_Drop_In
+#define COCOAPODS_VERSION_MAJOR_Braintree_Drop_In 3
+#define COCOAPODS_VERSION_MINOR_Braintree_Drop_In 6
+#define COCOAPODS_VERSION_PATCH_Braintree_Drop_In 1
+
+// Braintree/PayPal
+#define COCOAPODS_POD_AVAILABLE_Braintree_PayPal
+#define COCOAPODS_VERSION_MAJOR_Braintree_PayPal 3
+#define COCOAPODS_VERSION_MINOR_Braintree_PayPal 6
+#define COCOAPODS_VERSION_PATCH_Braintree_PayPal 1
+
+// Braintree/Payments
+#define COCOAPODS_POD_AVAILABLE_Braintree_Payments
+#define COCOAPODS_VERSION_MAJOR_Braintree_Payments 3
+#define COCOAPODS_VERSION_MINOR_Braintree_Payments 6
+#define COCOAPODS_VERSION_PATCH_Braintree_Payments 1
+
+// Braintree/UI
+#define COCOAPODS_POD_AVAILABLE_Braintree_UI
+#define COCOAPODS_VERSION_MAJOR_Braintree_UI 3
+#define COCOAPODS_VERSION_MINOR_Braintree_UI 6
+#define COCOAPODS_VERSION_PATCH_Braintree_UI 1
+
+// Braintree/Venmo
+#define COCOAPODS_POD_AVAILABLE_Braintree_Venmo
+#define COCOAPODS_VERSION_MAJOR_Braintree_Venmo 3
+#define COCOAPODS_VERSION_MINOR_Braintree_Venmo 6
+#define COCOAPODS_VERSION_PATCH_Braintree_Venmo 1
+
